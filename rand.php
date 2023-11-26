5cc3f609d60ee1838aa19d4dd1fc5556ac31dbf3
Karthika-O <147071387+Karthika-O@users.noreply.github.com>
2023-11-26 16:16:30 +0530

    Add files via upload

---
+++ rand.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Random Number</title>
+    </head>
+    <body>
+        <?php
+            $random=rand(1,100);
+            echo "<h1> $random </h1>";
+        ?>
+    </body>
+</html>
